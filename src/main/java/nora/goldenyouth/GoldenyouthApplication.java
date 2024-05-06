9f62f0c572bd659ba87631ee01344caa4a00590e
2522001 <minseo770@gmail.com>
2024-05-06 13:29:41 +0900

    init: #1 - 프로젝트 생성

---
+++ src/main/java/nora/goldenyouth/GoldenyouthApplication.java
@@ -0,0 +1,13 @@
+package nora.goldenyouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenyouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenyouthApplication.class, args);
+	}
+
+}
